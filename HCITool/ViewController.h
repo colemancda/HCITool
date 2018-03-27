76a221ffa878ddc288d3b5ed7a8429d938312477
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-27 11:29:00 -0500

    Added HCI event delegate

--- HCITool/ViewController.h
+++ HCITool/ViewController.h
@@ -10,21 +10,4 @@
 
 @interface ViewController : NSViewController
 
-
 @end
-
-//int BluetoothHCIDispatchUserClientRoutine(void * arg0, void * arg1, size_t * arg2);
-
-//int sendRawHCIRequest(uint8 * arg0, int arg1, void * arg2, size_t * arg3);
-/*
-SendRawHCICommand(    BluetoothHCIRequestID    inID,
-                  char *                      buffer,
-                  IOByteCount                bufferSize );
-*/
-
-/*
-int BluetoothHCISendRawCommand(const void *inputStruct, void * arg1, size_t * arg2);
-
-int IOBluetoothCSRLibHCISendBCCMDMessage(int arg0);
-*/
-
--- HCITool/ViewController.m
+++ HCITool/ViewController.m
@@ -6,23 +6,40 @@
 //  Copyright © 2018 Pure Swift. All rights reserved.
 //
 
-#import "ViewController.h"
-#import "IOBluetoothHostController.h"
 @import IOKit;
 @import IOBluetooth;
 
-struct BluetoothCall {
+#import <stdlib.h>
+#import <objc/objc.h>
+@import ObjectiveC;
+
+#import "ViewController.h"
+#import "IOBluetoothHostController.h"
+
+struct IOBluetoothHCIDispatchParams {
     uint64_t args[7];
     uint64_t sizes[7];
     uint64_t index;
 };
 
-struct BluetoothCallB {
-    uint8 a[116];
+struct BluetoothHCIUserClientNotificationDataInfo {
+    unsigned long long _field1;
+    unsigned long long _field2;
+    struct BluetoothHCIRequestCallbackInfo _field3;
+    unsigned int parameterSize;
+    unsigned int _field5;
+    unsigned short opcode;
+    unsigned char _field7;
+    unsigned char _field8;
+    unsigned char _field9;
+    unsigned char _field10;
+    unsigned char _field11;
+    unsigned char _field12;
 };
 
-struct HCIRequest {
-    uint32 identifier;
+struct IOBluetoothHCIEventNotificationMessage {
+    struct BluetoothHCIUserClientNotificationDataInfo dataInfo;
+    void *eventParameterData;
 };
 
 @interface ViewController ()
@@ -31,18 +48,31 @@ struct HCIRequest {
 
 @property (nonatomic, weak) IBOutlet NSTextField *messageLabel;
 
+@property (nonatomic) _IOBluetoothHostController *hciController;
+
 @end
 
 @implementation ViewController
 
++ (void)initialize {
+    /*
+    Method originalMethod = class_getInstanceMethod(IOBluetoothHostController.class, @selector(processRawEventData:dataSize:));
+    
+    Method newMethod = class_getInstanceMethod(self.class, @selector(_processRawEventData:dataSize:));
+    
+    method_exchangeImplementations(originalMethod, newMethod);
+    */
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _IOBluetoothHostController *hciController = [IOBluetoothHostController defaultController];
+    self.hciController = [IOBluetoothHostController defaultController];
+    self.hciController.delegate = self;
     
-    NSString *address = [hciController addressAsString];
+    NSString *address = [self.hciController addressAsString];
     
-    NSString *addressMessage = [NSString stringWithFormat:@"Address: %@ %@", address, hciController.className];
+    NSString *addressMessage = [NSString stringWithFormat:@"%@ %@ %@", address, self.hciController.className, self.hciController.nameAsString];
     
     NSLog(@"%@", addressMessage);
     
@@ -50,21 +80,22 @@ struct HCIRequest {
     
     //[self readConnectionTimeout:nil];
     //[self writeName:nil];
+    //[self scan:nil];
+    
+    // int _IOBluetoothNotificationLibNotificationCreate(int arg0, int arg1, int arg2, int arg3, int arg4)
+    //IOBluetoothNotificationLibNotificationCreate()
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+- (IBAction)scan:(id)sender {
+    
+    [self.hciController BluetoothHCILESetScanEnable:YES filterDuplicates:NO];
 }
 
 - (IBAction)readConnectionTimeout:(id)sender {
-    
-    _IOBluetoothHostController *hciController = [IOBluetoothHostController defaultController];
-    
+        
     uint16 connectionAcceptTimeout = 0;
     
-    int readTimeout = [hciController BluetoothHCIReadConnectionAcceptTimeout:&connectionAcceptTimeout];
+    int readTimeout = [self.hciController BluetoothHCIReadConnectionAcceptTimeout:&connectionAcceptTimeout];
     
     if (readTimeout) {
         
@@ -76,19 +107,19 @@ struct HCIRequest {
     
     // manually
     
-    struct HCIRequest request;
-    uint16 output;
+    BluetoothHCIRequestID request = 0;
+    uint16 output = 0;
     size_t outputSize = sizeof(output);
     
     int error = BluetoothHCIRequestCreate(&request, 1000, nil, 0);
     
-    NSLog(@"Created request: %u", request.identifier);
+    NSLog(@"Created request: %u", request);
     
     if (error) {
         
         BluetoothHCIRequestDelete(request);
         
-        printf("Couldnt create error: %08x\n", error);
+        printf("Couldn't create error: %08x\n", error);
     }
     
     size_t commandSize = 3;
@@ -118,15 +149,13 @@ struct HCIRequest {
 
 - (IBAction)writeName:(id)sender {
     
-    _IOBluetoothHostController *hciController = [IOBluetoothHostController defaultController];
-    
     unsigned char name[256];
     
     name[0] = 'C';
     name[1] = 'D';
     name[2] = 'A';
     
-    int setNameError = [hciController BluetoothHCIWriteLocalName:&name];
+    int setNameError = [self.hciController BluetoothHCIWriteLocalName:&name];
     
     if (setNameError) {
         
@@ -138,10 +167,10 @@ struct HCIRequest {
     
     // manually
     
-    struct HCIRequest request;
+    BluetoothHCIRequestID request;
     int error = BluetoothHCIRequestCreate(&request, 1000, nil, 0);
     
-    NSLog(@"Created request: %lu", request.identifier);
+    NSLog(@"Created request: %lu", request);
     
     if (error) {
         
@@ -178,9 +207,32 @@ struct HCIRequest {
     self.messageLabel.stringValue = message;
 }
 
+- (void)_processRawEventData:(uint8 *)bytes
+                    dataSize:(size_t)size {
+    
+    // call original implementation
+    //[_hciController processRawEventData:data dataSize:size];
+    
+    NSData *data = [NSData dataWithBytes:bytes length:size];
+    
+    NSLog(@"HCI Event  %@", data);
+    
+    
+}
+
+- (void)BluetoothHCIEventNotificationMessage:(IOBluetoothHostController*)controller
+                       inNotificationMessage:(struct IOBluetoothHCIEventNotificationMessage *)message {
+    
+    size_t size = message->dataInfo.parameterSize;
+    
+    NSData *data = [NSData dataWithBytes:&message->eventParameterData length:size];
+    
+    NSLog(@"HCI Event %16x %@", message->dataInfo.opcode, data);
+}
+
 @end
 
-int _BluetoothHCISendRawCommand(struct HCIRequest request,
+int _BluetoothHCISendRawCommand(BluetoothHCIRequestID request,
                                 void *commandData,
                                 size_t commmandSize,
                                 void *returnParameter,
@@ -188,7 +240,7 @@ int _BluetoothHCISendRawCommand(struct HCIRequest request,
     
     int errorCode = 0;
     
-    struct BluetoothCall call;
+    struct IOBluetoothHCIDispatchParams call;
     size_t size = 0x74;
     memset(&call, 0x0, size);
     
@@ -196,7 +248,7 @@ int _BluetoothHCISendRawCommand(struct HCIRequest request,
         
         // IOBluetoothHostController::
         // SendRawHCICommand(unsigned int, char*, unsigned int, unsigned char*, unsigned int)
-        call.args[0] = (uintptr_t)&request.identifier;
+        call.args[0] = (uintptr_t)&request;
         call.args[1] = (uintptr_t)commandData;
         call.args[2] = (uintptr_t)&commmandSize;
         call.sizes[0] = sizeof(uint32);
