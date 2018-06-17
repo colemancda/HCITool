afd7597180ce8024ff84c028dab672455a23dc69
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-17 09:49:43 -0500

    Working on HCI events

--- HCITool/ViewController.m
+++ HCITool/ViewController.m
@@ -85,6 +85,11 @@ struct IOBluetoothHCIEventNotificationMessage {
 - (IBAction)scan:(id)sender {
     
     [self.hciController BluetoothHCILESetScanEnable:YES filterDuplicates:NO];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [self.hciController BluetoothHCILESetScanEnable:NO filterDuplicates:YES];
+    });
 }
 
 - (IBAction)readConnectionTimeout:(id)sender {
